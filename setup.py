880119a0c043796b83f58d9242e99566fff82b1a
cantixcrew <49995452+cantixcrew@users.noreply.github.com>
2020-06-10 12:19:38 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     entry_points={"console_scripts": ["s3lowbe=s3lowbe:main"]},
     version="0.2.0",
     description="Low bandwidth DoS tool. s3lowbe rewrite in Python.",
-    author="Gokberk Yaltirakli",
-    author_email="webdosusb@gmail.com",
+    author="Rex4",
+    author_email="cantixcr3w@yahoo.com",
     url="https://github.com/cantix/s3lowbe",
     keywords=["dos", "http", "s3lowbe"],
 )
